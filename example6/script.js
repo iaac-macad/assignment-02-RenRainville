4f9272930bf81d8b1e377478a23f5c869598a8c9
RenRainville <mark.rainville@students.iaac.net>
2023-01-30 16:36:09 -0800

    set initial camera position to Z = 100

--- example6/script.js
+++ example6/script.js
@@ -19,6 +19,7 @@ function init() {
     scene.background = new THREE.Color(0.8,0.8,0.8)
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2500 )
     camera.position.y = - 100
+    camera.position.z = 100
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
